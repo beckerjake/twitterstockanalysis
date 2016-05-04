0d92b6586ee28c051b7376ec553cbc3be8a4f59d
Jake Becker <jkbckr@gmail.com>
2016-05-04 09:06:51 +0000

    Parse stock price JSON data and insert into MySQL database

--- finance.js
+++ finance.js
@@ -1,6 +1,16 @@
 #!/usr/local/bin/node
 
-var request = require("request")
+var request = require('request')
+var mysql = require('mysql')
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: process.env.MYSQL_PASSWD,
+	database: 'ticktalk'
+	});
+
+connection.connect();
 
 var url = "http://finance.yahoo.com/webservice/v1/symbols/BA,UNH,WFC,T,BP,PCG,KO,IBM,MSFT,MAR/quote?format=json&view=detail"
 
@@ -12,9 +22,32 @@ request({
 		var stockDataList = body["list"]["resources"];
 		for (var i = 0; i < stockDataList.length; i++) {
 			var fields = stockDataList[i]["resource"]["fields"];
-			console.log(fields["name"] + ": " + fields["price"]);
+
+			var issuer_name = fields["issuer_name"];
+			var symbol = fields["symbol"];
+			var price = parseFloat(fields["price"]);
+			var ts = parseInt(fields["ts"]);
+			var time_alt = fields["ts"];
+			var change = parseFloat(fields["change"]);
+			var chg_percent = parseFloat(fields["chg_percent"]);
+			var day_high = parseFloat(fields["day_high"]);
+			var day_low = parseFloat(fields["day_low"]);
+			var year_high = parseFloat(fields["year_high"]);
+			var year_low = parseFloat(fields["year_low"]);
+			var type = fields["type"];
+			var volume = parseInt(fields["volume"]);
+
+			connection.query('INSERT INTO stocks (issuer_name, symbol, price, ts, time_alt, `change`, chg_percent, day_high, day_low, year_high, year_low, type, volume) VALUES (\''+issuer_name+'\',\''+symbol+'\','+price+','+ts+',FROM_UNIXTIME(\''+time_alt+'\'),'+change+','+chg_percent+','+day_high+','+day_low+','+year_high+','+year_low+',\''+type+'\','+volume+')', function(err, rows, data) {
+				if (err) {
+					console.log("Error inserting stock data into database.");
+					console.log(err);			
+				}
+			});
+		
+			if (i == stockDataList.length - 1) {
+				connection.end();
+			}
 		}
 	}
-})
-
+});
 
