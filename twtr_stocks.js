6a5e60d3e5ab82db40fd600b8791b592c3ebad28
Jake Becker <jkbckr@gmail.com>
2016-05-05 09:17:32 +0000

    Twiter library appears to crash if attempting to track >2 stocks

--- twtr_stocks.js
+++ twtr_stocks.js
@@ -9,15 +9,108 @@ var client = new Twitter({
    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-var match = /(\$BA|\$UNH|\$WFC|\$T|\$BP|\$PCG|\$KO|\$IBM|\$MSFT|\$MAR)/;
-var match2 = '\$MSFT'
+//var stocksToTrack = ['\$BA', '\$UNH', '\$WFC', '\$T', '\$BP', '\$PCG', '\$KO', '\$IBM', '\$MSFT', '\$MAR'];
 
-client.stream('statuses/filter', {track: match2}, function(stream) {
-    stream.on('data', function(tweet) {
-        console.log(tweet);
-    });
+function storeTweetInDB(sym, twt) {
+	console.log(sym + ": " + twt.text);
+}
+
+client.stream('statuses/filter', {track: '\$BA'}, function(stream) {
+	stream.on('data', function(tweet) {
+        storeTweetInDB('\$BA', tweet);
+   	});
     
     stream.on('error', function(error) {
         throw error;
     });
 });
+
+client.stream('statuses/filter', {track: '\$UNH'}, function(stream) {
+	stream.on('data', function(tweet) {
+		storeTweetInDB('\$UNH', tweet);
+	}); 
+
+	stream.on('error', function(error) {
+		throw error;
+	}); 
+});
+/*
+client.stream('statuses/filter', {track: '\$WFC'}, function(stream) {
+	stream.on('data', function(tweet) {
+		storeTweetInDB('\$WFC', tweet);
+	}); 
+
+	stream.on('error', function(error) {
+		throw error;
+	}); 
+}); 
+
+client.stream('statuses/filter', {track: '\$T'}, function(stream) {
+	stream.on('data', function(tweet) {
+		storeTweetInDB('\$T', tweet);
+	}); 
+
+	stream.on('error', function(error) {
+		throw error;
+	});
+}); 
+
+client.stream('statuses/filter', {track: '\$BP'}, function(stream) {
+	stream.on('data', function(tweet) {
+		storeTweetInDB('\$BP', tweet);
+	}); 
+
+	stream.on('error', function(error) {
+		throw error;
+	}); 
+}); 
+
+client.stream('statuses/filter', {track: '\$PCG'}, function(stream) {
+	stream.on('data', function(tweet) {
+		storeTweetInDB('\$PCG', tweet);
+	}); 
+
+	stream.on('error', function(error) {
+		throw error;
+	});
+}); 
+
+client.stream('statuses/filter', {track: '\$KO'}, function(stream) {
+	stream.on('data', function(tweet) {
+		storeTweetInDB('\$KO', tweet);
+	}); 
+
+	stream.on('error', function(error) {
+		throw error;
+	}); 
+}); 
+
+client.stream('statuses/filter', {track: '\$IBM'}, function(stream) {
+	stream.on('data', function(tweet) {
+		storeTweetInDB('\$IBM', tweet);
+	}); 
+
+	stream.on('error', function(error) {
+		throw error;
+	});
+});
+
+client.stream('statuses/filter', {track: '\$MSFT'}, function(stream) {
+	stream.on('data', function(tweet) {
+		storeTweetInDB('\$MSFT', tweet);
+	});
+
+	stream.on('error', function(error) {
+		throw error;
+	});
+});
+
+client.stream('statuses/filter', {track: '\$MAR'}, function(stream) {
+	stream.on('data', function(tweet) {
+		storeTweetInDB('\$MAR', tweet);
+	});
+
+	stream.on('error', function(error) {
+	      throw error;
+    });
+});*/
