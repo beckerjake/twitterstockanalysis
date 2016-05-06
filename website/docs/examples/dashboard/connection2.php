5ba109706191a0ececc423d5181227465d19fcd2
Sean Oswald <Sean Oswald>
2016-05-05 20:20:30 -0700

    Initital Commit: I got a whole lotta shit in here. Might need to trim down later, but this is easiest for now, and works.

---
+++ website/docs/examples/dashboard/connection2.php
@@ -0,0 +1,39 @@
+<?php 
+/*$con=mysql_connect('ec2-52-27-111-35.us-west-2.compute.amazonaws.com',"","");  
+if (!$con)
+{
+    die('Could not connect: ' . mysql_error());
+} */  
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ticktalk";
+
+
+// Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+$sql = "SELECT *";
+$result = mysqli_query($con, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+
+
+?>
