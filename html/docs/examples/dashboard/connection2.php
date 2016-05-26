d9575eb850167862bc8057dc668d8ba8b08da6da
Sean Oswald <Sean Oswald>
2016-05-25 20:20:01 -0700

    changed the mysql calls to get accurate information

--- html/docs/examples/dashboard/connection2.php
+++ html/docs/examples/dashboard/connection2.php
@@ -5,7 +5,7 @@ if (!$con)
     die('Could not connect: ' . mysql_error());
 } */  
 
-
+$numStocks = 10;
 
 $servername = "localhost";
 $username = "root";
@@ -17,36 +17,61 @@ $dbname = "ticktalk";
 $con = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$con) {
+if (!$con) 
+{
     die("Connection failed: " . mysqli_connect_error());
 }
 
 
-$sql = "SELECT * FROM  ticktalk.stocks";
+//$sql = "SELECT TOP " .$numStocks. " * FROM  ticktalk.stocks";
+//$sql = "SELECT * FROM (SELECT * FROM ticktalk.daySummaries order by id desc limit ".$numStocks.") unsorted order by id asc";
+//$sql = "SELECT * FROM (SELECT * FROM ticktalk.daySummaries order by id desc limit ".$numStocks.") unsorted order by stockName asc";
+$sql = "SELECT * FROM (SELECT * FROM ticktalk.daySummaries order by id desc limit ".$numStocks.") unsorted order by trim(leading 'the ' from lower(stockName)) asc";
 $result = mysqli_query($con, $sql);
 
-if (mysqli_num_rows($result) > 0) {
+if (mysqli_num_rows($result) > 0) 
+{
     // output data of each row
-			echo "<table border='1' >
-<tr>
-<td align=center> <b>Company No</b></td>
-<td align=center><b>Ticker</b></td>
-<td align=center><b>Price</b></td>";
+	echo "<table border='1' >
+			<tr>
+			<td align=center> <b>Company No</b></td>
+			<td align=center><b>Ticker</b></td>
+			<td align=center><b>Price</b></td>";
     while($row = mysqli_fetch_assoc($result)) {
 		  
-    echo "<tr>";
-    echo "<td align=center>".$row["issuer_name"]."</td>";
-    echo "<td align=center>".$row["symbol"]."</td>";
-    echo "<td align=center>".$row["price"]."</td>";
-    //echo "<td align=center>$data[3]</td>";
-    //echo "<td align=center>$data[4]</td>";
-    echo "</tr>";
-}
-echo "</table>";
+		  
+		  echo "<tr>";
+          echo "        <td>".$row["stockName"]."</td>";
+          echo "        <td>".$row["symbol"]."</td>";
+          echo "        <td>Neutral</td>";
+          echo "        <td>".$row["price"]."</td>";
+          echo "        <td><a href=\"#\" id=\"show_1\">Show Data</a></td>";
+          echo "      </tr>";
+          echo "      <tr>";
+          echo "        <td colspan=\"5\">";
+          echo "          <div id=\"extra_1\" style=\"display: none;\">";
+          echo "            <br>hidden row";
+          echo "            <br>hidden row";
+          echo "            <br>hidden row";
+          echo "          </div>";
+          echo "        </td>";
+          echo "      </tr>";
+		  
+		  /*
+			echo "<tr>";
+			echo "<td align=center>".$row["issuer_name"]."</td>";
+			echo "<td align=center>".$row["symbol"]."</td>";
+			echo "<td align=center>".$row["price"]."</td>";
+			echo "</tr>";
+	*/
+	}
+
+	echo "</table>";
 		
        // echo "Company: " . $row["issuer_name"]. " - Ticker: " . $row["symbol"]. " - Price: " . $row["price"]. "<br>";
-    }
- else {
+}
+ else 
+{
     echo "0 results";
 }
 
