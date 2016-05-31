890cb4b0969560c93f6f2aba1afac9af0a6f11b4
Sean Oswald <Sean Oswald>
2016-05-30 17:32:47 -0700

    display 2 medium graphs, centered

--- html/docs/examples/dashboard/connection2.php
+++ html/docs/examples/dashboard/connection2.php
@@ -65,9 +65,9 @@ if (mysqli_num_rows($result) > 0)
           echo "          <div id=\"extra_".$counter."\" style=\"display: none;\">";
           //echo "            <img src=\"http://chart.finance.yahoo.com/z?s=".$symbol."&t=".$time_period."&q=l&l=on&z=m\"/>";
 		  //three graph display
-		  echo "            <img src=\"http://chart.finance.yahoo.com/z?s=".$symbol."&t=7d&q=l&l=on&z=s\"/>";
-		  echo "            <img src=\"http://chart.finance.yahoo.com/z?s=".$symbol."&t=1m&q=l&l=on&z=s\"/>";
-		  echo "            <img src=\"http://chart.finance.yahoo.com/z?s=".$symbol."&t=1y&q=l&l=on&z=s\"/>";
+		  echo "            <img src=\"http://chart.finance.yahoo.com/z?s=".$symbol."&t=7d&q=l&l=on&z=m\"/>";
+		  echo "            <img src=\"http://chart.finance.yahoo.com/z?s=".$symbol."&t=1m&q=l&l=on&z=m\"/>";
+		  //echo "            <img src=\"http://chart.finance.yahoo.com/z?s=".$symbol."&t=1y&q=l&l=on&z=s\"/>";
           
 		  echo "            <br>hidden row";
           echo "            <br>hidden row";
